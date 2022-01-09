2251a3bcec6592bfb29f145efb12b38769be0e97
Nicolas <nicolas.bautista.rojas97@gmail.com>
2022-01-08 19:27:38 -0500

    Cambios de Jhoan Estilo Menu

--- src/componentes/odontologos/Odontologos.js
+++ src/componentes/odontologos/Odontologos.js
@@ -1,101 +1,101 @@
-import React, { useEffect, useState, useContext, Fragment } from 'react';
+import React, { useEffect, useState, useContext, Fragment } from "react";
 
 // importar cliente axios
-import odontologoAxios from '../../config/axios';
-import Odontologo from './Odontologo';
-import Spinner from '../layout/Spinner';
-import { Link, withRouter } from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import odontologoAxios from "../../config/axios";
+import Odontologo from "./Odontologo";
+import Spinner from "../layout/Spinner";
+import { Link, withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 // import el Context
-import { CRMContext } from '../../context/CRMContext';
+import { CRMContext } from "../../context/CRMContext";
 
 function Odontologos(props) {
-    // Trabajar con el state
-    // Odontologos = state,  guardarOdontologos = funcion para guardar el state
-    const [ odontologos, guardarOdontologos ] = useState([]);
+  // Trabajar con el state
+  // Odontologos = state,  guardarOdontologos = funcion para guardar el state
+  const [odontologos, guardarOdontologos] = useState([]);
 
-    // utilizar valores del context
-    const [auth, guardarAuth ] = useContext( CRMContext );
+  // utilizar valores del context
+  const [auth, guardarAuth] = useContext(CRMContext);
 
-    // use effect es similar a componentdidmount y willmount
-    useEffect( () => {
-
-        if(auth.token !== '') {
-            // Query a la API
-            const consultarAPI = async () => {
-                try {
-                    const odontologosConsulta = await odontologoAxios.get('/api/odontologo', {
-                        headers: {
-                            Authorization : `Bearer ${auth.token}`
-                        }
-                    });
-                    console.log(odontologosConsulta.data);
-                    // colocar el resultado en el state
-                    guardarOdontologos(odontologosConsulta.data["odontologos"]);
-
-                } catch (error) {
-                    // Error con authorizacion
-                    if(error.response.status = 500) {
-                        props.history.push('/iniciar-sesion');
-                    }
-                }
+  // use effect es similar a componentdidmount y willmount
+  useEffect(() => {
+    if (auth.token !== "") {
+      // Query a la API
+      const consultarAPI = async () => {
+        try {
+          const odontologosConsulta = await odontologoAxios.get(
+            "/api/odontologo",
+            {
+              headers: {
+                Authorization: `Bearer ${auth.token}`,
+              },
             }
-            consultarAPI();
-        } else {
-            props.history.push('/iniciar-sesion');
+          );
+          console.log(odontologosConsulta.data);
+          // colocar el resultado en el state
+          guardarOdontologos(odontologosConsulta.data["odontologos"]);
+        } catch (error) {
+          // Error con authorizacion
+          if ((error.response.status = 500)) {
+            props.history.push("/iniciar-sesion");
+          }
         }
-    }, [odontologos] );
-
-
-    // Si el state esta como false
-    if(!auth.auth) {
-        props.history.push('/iniciar-sesion');
+      };
+      consultarAPI();
+    } else {
+      props.history.push("/iniciar-sesion");
     }
+  }, [odontologos]);
 
-    if(!odontologos.length) return <Spinner /> 
+  // Si el state esta como false
+  if (!auth.auth) {
+    props.history.push("/iniciar-sesion");
+  }
 
-    
-    return (
-      <Fragment>
-        <h2>Odontólogos</h2>
-        
+  if (!odontologos.length) return <Spinner />;
+
+  return (
+    <Fragment>
+      <h2>Odontólogos</h2>
+
+      <div className="btnAdd">
         <Link to={"/odontologos/nuevo"}>
-          <div className="btnAdd">
-            <FontAwesomeIcon icon={faPlus} className="add"></FontAwesomeIcon>
-            <p className="add" id="addM">Agregar Médico</p>
-          </div>
-          
+          <FontAwesomeIcon icon={faPlus} className="add"></FontAwesomeIcon>
+          <p className="add" id="addM">
+            Agregar Médico
+          </p>
         </Link>
-        <div className="contenedorTable">
-          <div className="tableTitle">
-            <h2>Odontólogos</h2>
-          </div>
-          <div className="tableModel">
-            <div className="table-responsive">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Nombre</th>
-                    <th>Correo</th>
-                    <th>Telefono</th>
-                    <th>Documento</th>
-                    <th>Horario</th>
-                    <th>Acciones</th>
-                  </tr>
-                </thead> 
-                <tbody>
+      </div>
+
+      <div className="contenedorTable">
+        <div className="tableTitle">
+          <h2>Odontólogos</h2>
+        </div>
+        <div className="tableModel">
+          <div className="table-responsive">
+            <table>
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Correo</th>
+                  <th>Telefono</th>
+                  <th>Documento</th>
+                  <th>Horario</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
                 {odontologos.map((odontologo) => (
-                    <Odontologo key={odontologo._id} odontologo={odontologo} />
+                  <Odontologo key={odontologo._id} odontologo={odontologo} />
                 ))}
-                </tbody>
-              </table>
-            </div>
+              </tbody>
+            </table>
           </div>
         </div>
-        
-      </Fragment>
-    );
+      </div>
+    </Fragment>
+  );
 }
 export default withRouter(Odontologos);
