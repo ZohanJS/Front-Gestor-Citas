e45ac40e5044a4107ebb08fbf87924289d1a8e62
Nicolas <nicolas.bautista.rojas97@gmail.com>
2022-01-16 14:01:50 -0500

    Botón agregar crud  tablas

--- src/componentes/odontologos/Odontologos.js
+++ src/componentes/odontologos/Odontologos.js
@@ -1,18 +1,31 @@
 import React, { Fragment } from "react";
 import Odontologo from "./Odontologo";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import NuevoOdontologo from "./NuevoOdontologo";
-import { useOdontologosController } from './hooks/UseOdontologosController'
+import { useOdontologosController } from "./hooks/UseOdontologosController";
 import Spinner from "../layout/Spinner";
 
+
+
 function Odontologos() {
-  const {odontologos} =  useOdontologosController();
+  const { odontologos } = useOdontologosController();
 
   if (!odontologos.length) return <Spinner />;
 
   return (
     <Fragment>
-      <h2>Odontólogos</h2>
-      <NuevoOdontologo />
+      
+      {/*    <NuevoOdontologo /> */}
+      <div className="btnAdd">
+        <Link to={"/odontologos/nuevo"}>
+          <FontAwesomeIcon icon={faPlus} className="add"></FontAwesomeIcon>
+          <p className="add" id="addM">
+            Agregar Médico
+          </p>
+        </Link>
+      </div>      
       <div className="contenedorTable">
         <div className="tableTitle">
           <h2 id="tableTitle">Odontólogos</h2>
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const odontologoAxios = axios.create({
-    baseURL : 'http://192.168.0.7:4001'
+    baseURL : 'http://127.0.0.1:4001'
 });
 
 export default odontologoAxios;
