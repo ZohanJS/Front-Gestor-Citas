1230b6436cbab1f1cef49f2a875b9f1c56b704a1
ZohanJS <amolasempanadas123@gmail.com>
2022-02-21 19:56:29 -0500

    Preparacion despliegue

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const odontologoAxios = axios.create({
-    baseURL : 'http://127.0.0.1:4001'
+    baseURL : 'https://backend-gestor-citas.herokuapp.com'
 });
 
 export default odontologoAxios;
